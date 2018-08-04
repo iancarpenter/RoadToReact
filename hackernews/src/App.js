b2946f3af65a6d6124fa840053109a18b7359d4a
Ian Carpenter <ianhcarpenter@gmail.com>
2018-08-04 21:16:57 +0100

    Unidirection Chapter

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -21,18 +21,45 @@ const list =  [
 ]
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list,      
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    function isNotId(item) {
+      return item.objectID !== id;      
+    };
+
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({list: updatedList});
+  }
+
   render() {    
     return (
       <div className="App">
-        {list.map(item => 
+        {this.state.list.map(item => 
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
               </span>
               <span>{item.author}</span>
               <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>        
+              <span>{item.points}</span>              
+              <span>
+                <button 
+                   onClick={()=> this.onDismiss(item.objectID)}
+                   type="button">
+                   Dismiss
+                </button>
+              </span>              
+            </div>                   
         )}
       </div>
       
